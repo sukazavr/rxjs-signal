db6643a581544daa96329119f9da6fc03fa0df96
Dmitrii Bykov <sukazavr@gmail.com>
2021-04-10 22:19:31 +0700

    feat: add cs (create signal)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,69 @@
-console.log('hello world')
+import { Observable, Subject, Subscription } from 'rxjs'
+import { filter, map, share } from 'rxjs/operators'
+
+export interface Signal<P1 = void, P2 = P1, R = void> {
+  $: Observable<P2>
+  _: (...args: P1 extends void ? unknown[] : [P1]) => () => R
+  (...args: P1 extends void ? unknown[] : [P1]): R
+}
+
+export type SignalModifier<P1, P2, R> = (emit: (payloadOutput: P2) => void, payloadInput: P1) => R
+
+export interface SignalWave {
+  signal: Signal<unknown>
+  payload: unknown
+}
 
 /**
- * Does the foobar thing.
+ * Create Signal which is function with static props `$` = shared Observable<P2> and `_` =
+ * (payload: P1) => () => self()
  *
+ * Signal<P1 = void, P2 = P1, R = void>
+ * P1 is input payload type, P2 is output payload type, R is return type.
+ *
+ * @function cs
+ * @param {function(emit: function(payloadOutput: P2): void, payloadInput: P1): R} [modifier]
+ * An optional function to map payload on other value and produce custom value for return from `cs` after the
+ * call.
+ * @returns {Signal<P1 = void, P2 = P1, R = void>}
  * @example
  *
- * const a = foobar({
- *  a: 1
- *    b: 2
- * })
+ *   import { ca } from 'rxjs-signal'
+ *
+ *   const save = ca<{ data: string }>()
+ *   save.$.subscribe((x) => console.log(x))
+ *   save({ data: 'hey' })
  *
  */
-export function foobar(): null {
-  return null
+export function cs(): Signal
+export function cs<P1>(): Signal<P1>
+export function cs<P1>(modifier: SignalModifier<P1, P1, void>): Signal<P1>
+export function cs<P1, R>(modifier: SignalModifier<P1, P1, R>): Signal<P1, P1, R>
+export function cs<P1, P2, R>(modifier: SignalModifier<P1, P2, R>): Signal<P1, P2, R>
+export function cs<P1, P2, R>(modifier?: SignalModifier<P1, P2, R>): Signal<P1, P2, R> {
+  const signal = (typeof modifier === 'function'
+    ? (payload) => modifier((payload) => signalWave$.next({ signal, payload }), payload as P1)
+    : (payload) => signalWave$.next({ signal, payload })) as Signal<unknown>
+  signal._ = (payload) => () => signal(payload)
+  signal.$ = signalWave$.pipe(
+    filter((v) => v.signal === signal),
+    map((v) => v.payload),
+    share()
+  )
+  return (signal as unknown) as Signal<P1, P2, R>
 }
+
+export const signalWave$ = new Subject<SignalWave>()
+
+/**
+ * Signal debug helper.
+ *
+ * @function signalDebug
+ * @returns {Subscription}
+ */
+export const signalDebug = (): Subscription =>
+  signalWave$.subscribe(({ signal, payload }) => {
+    console.group('🔷', signal, '🔹')
+    console.dir(payload, { colors: true })
+    console.groupEnd()
+  })
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,3 +1,102 @@
-it('works', () => {
-  expect(true).toEqual(true)
+import { Compiler, expecter } from 'ts-snippet'
+import { cs } from '../src'
+import { take } from 'rxjs/operators'
+
+export const compiler: Compiler = (global as any).window ? undefined! : new Compiler({ strict: true })
+
+describe('', () => {
+  it('should receive value from cs().$', () => {
+    expect.assertions(1)
+    const signal = cs<number>()
+    const result = signal.$.pipe(take(1)).toPromise()
+    signal(1)
+    expect(result).resolves.toEqual(1)
+  })
+})
+
+describe('types', () => {
+  const expectSnippet = expecter(
+    (code) => `
+      import { cs } from "./src";
+      ${code}
+    `,
+    compiler
+  )
+
+  it('should succeed to call signal w/o args from cs()', () => {
+    expectSnippet(`
+      cs()();
+    `).toSucceed()
+  })
+
+  it('should succeed to call signal with many args from cs()', () => {
+    expectSnippet(`
+      cs()(1,2,3,4,5,6,7,8);
+    `).toSucceed()
+  })
+
+  it('should succeed to infer types from cs()', () => {
+    const snippet = expectSnippet(`
+      const signal = cs();
+      const $ = signal.$;
+      const _ = signal._();
+      const result = signal();
+    `)
+    snippet.toInfer('signal', 'Signal<void, void, void>')
+    snippet.toInfer('$', 'Observable<void>')
+    snippet.toInfer('_', '() => void')
+    snippet.toInfer('result', 'void')
+  })
+
+  it('should fail to call signal w/o args from cs<number>()', () => {
+    expectSnippet(`
+      cs<number>()();
+    `).toFail()
+  })
+
+  it('should fail to call signal with many args from cs<number>()', () => {
+    expectSnippet(`
+      cs<number>()(1,2,3,4,5,6,7,8);
+    `).toFail()
+  })
+
+  it('should succeed to call signal with one correct args from cs<number>()', () => {
+    expectSnippet(`
+      cs<number>()(1);
+    `).toSucceed()
+  })
+
+  it('should fail to call signal with one incorrect args from cs<number>()', () => {
+    expectSnippet(`
+      cs<number>()(null);
+    `).toFail()
+  })
+
+  it('should succeed to infer types from cs<number>()', () => {
+    const snippet = expectSnippet(`
+      const signal = cs<number>();
+      const $ = signal.$;
+      const _ = signal._(1);
+      const result = signal(1);
+    `)
+    snippet.toInfer('signal', 'Signal<number, number, void>')
+    snippet.toInfer('$', 'Observable<number>')
+    snippet.toInfer('_', '() => void')
+    snippet.toInfer('result', 'void')
+  })
+
+  it('should succeed to infer types from cs<string, number, (x: number) => void>()', () => {
+    const snippet = expectSnippet(`
+      const signal = cs<string, number, (x: number) => void>((emit, str) => (x) => emit(x + Number(str)));
+      const $ = signal.$;
+      const _ = signal._('string');
+      const result = signal('string');
+      const finalResult = result(1);
+    `)
+    snippet.toInfer('signal', 'Signal<string, number, (x: number) => void>')
+    snippet.toInfer('$', 'Observable<number>')
+    snippet.toInfer('_', '() => (x: number) => void')
+    snippet.toInfer('result', '(x: number) => void')
+    snippet.toInfer('finalResult', 'void')
+  })
 })
